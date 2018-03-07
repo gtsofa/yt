93d7f795992609c3a03678a9bb1be2e31619c849
gtsofa <jlsnyule@yahoo.com>
2018-03-07 19:08:36 +0300

    add cal.py

---
+++ cal.py
@@ -0,0 +1,25 @@
+# calculator class that will perform test on
+
+def add(x, y):
+    """Add function"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+
+def divide(x, y):
+    """Divide Function"""
+    if y == 0:
+        raise ValueError('Can not divide by zero!')
+    return x / y
+
+print(add(5,3))
+print(subtract(5,3))
+print(multiply(5,3))
+print(divide(5,3))
+
