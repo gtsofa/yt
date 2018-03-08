c747d656c848838c38adfcfb027a5253c407a8dc
gtsofa <jlsnyule@yahoo.com>
2018-03-08 21:17:59 +0300

    add changes

---
+++ primefactors.py
@@ -0,0 +1,14 @@
+
+def find_prime_facs(n):
+  list_of_factors=[]
+  i=2
+  while n>1:
+    
+    if n%i==0:
+      list_of_factors.append(i)
+      n=n/i
+      i=i-1
+    i+=1  
+  return list_of_factors
+  
+print(find_prime_facs(4))
--- test_primefactors.py
+++ test_primefactors.py
@@ -5,7 +5,14 @@ import unittest
 import primefactors
 
 class PrimeFactorsTestCase(unittest.TestCase):
-    pass
+    
+    @property
+    def test_factors(self):
+        list_of_factors = []
+        self.assertEqual(list_of_factors(1), [])
+        self.assertEqual(list_of_factors(2), [2])
+        self.assertEqual(list_of_factors(3), [3])
+        self.assertEqual(list_of_factors(4), [2,2])
 
 
 if __name__ == '__main__':
