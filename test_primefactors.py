db1ce33fadc4e88a1f97a15045d76ee476f6fa27
gtsofa <jlsnyule@yahoo.com>
2018-03-08 20:44:16 +0300

    initialized the test module

---
+++ test_primefactors.py
@@ -0,0 +1,12 @@
+# primefactors.py unit test cases
+
+import unittest
+
+import primefactors
+
+class PrimeFactorsTestCase(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
