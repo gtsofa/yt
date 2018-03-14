11d7b3f40073fbc132478a64369c8f32ab864306
gtsofa <jlsnyule@yahoo.com>
2018-03-14 17:44:56 +0300

    add smart_cal.py

---
+++ smart_cal.py
@@ -0,0 +1,8 @@
+# a smart calculator where the use presses even the operators
+
+# Enter calculations : 5 * 6 
+# 5 * 6 = 30
+# Store the user input of the two numbers and the operator
+# Convert the strings into integers
+# if + then we need to provide output based on addition
+# print the result
