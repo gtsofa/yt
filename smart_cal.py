54cd9b72d8e8e5f490e7c7e22aaf7b63608475bd
gtsofa <jlsnyule@yahoo.com>
2018-03-14 21:18:23 +0300

    fixed bug

--- smart_cal.py
+++ smart_cal.py
@@ -3,12 +3,17 @@
 # Enter calculations : 5 * 6 
 # 5 * 6 = 30
 # Store the user input of the two numbers and the operator
-num1, operator, num2 = input("Enter Calculation: ")
+#num1, operator, num2 = input("Enter Calculation:")
+#num1, num2 = input("enter numbers:").split()
+num1, operator, num2 = raw_input("enter numbers:").split()
 # Convert the strings into integers
 num1 = int(num1)
 num2 = int(num2)
 
-# if + then we need to provide output based on addition
+#print("{} {}".format(num1, num2))
+
+
+#if + then we need to provide output based on addition
 if operator == "+":
     print("{} + {} = {}".format(num1, num2, num1+num2))
 
@@ -23,4 +28,4 @@ elif operator == "/":
 
 else:
     print("use either + - * or / next time.")
-# print the result
+# # print the result
