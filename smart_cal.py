c071e89df8a6d4c8fcf219a4bd5975e6d937f165
gtsofa <jlsnyule@yahoo.com>
2018-03-14 18:07:54 +0300

    refactor

--- smart_cal.py
+++ smart_cal.py
@@ -3,6 +3,24 @@
 # Enter calculations : 5 * 6 
 # 5 * 6 = 30
 # Store the user input of the two numbers and the operator
+num1, operator, num2 = input("Enter Calculation: ")
 # Convert the strings into integers
+num1 = int(num1)
+num2 = int(num2)
+
 # if + then we need to provide output based on addition
+if operator == "+":
+    print("{} + {} = {}".format(num1, num2, num1+num2))
+
+elif operator == "-":
+    print("{} - {} = {}".format(num1, num2, num1-num2))
+
+elif operator == "*":
+    print("{} * {} = {}".format(num1, num2, num1*num2))
+
+elif operator == "/":
+    print("{} / {} = {}".format(num1, num2, num1/num2))
+
+else:
+    print("use either + - * or / next time.")
 # print the result
