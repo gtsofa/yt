f0ca410f7acfa9cc4dfe71a5a44f41d4a41be5d0
gtsofa <jlsnyule@yahoo.com>
2018-03-15 19:39:52 +0300

    refactor app

--- area_cirle.py
+++ area_cirle.py
@@ -1,6 +1,9 @@
 # area_circle.py
-import math
+
+#import math
 def areaCirlce(radius):
-    area = math.pi*radius**2
+    #area = math.pi*radius**2
+    # instead of math import we used actual val of pi as 3.141592653589793
+    area = 3.141592653589793 * radius**2
 
     return area
