07ccd16f494e0554aa0a096c12f702e0d8926ff8
gtsofa <jlsnyule@yahoo.com>
2018-03-15 19:15:02 +0300

    add test_areaCirlce.py

---
+++ test_area_circle.py
@@ -0,0 +1,20 @@
+# test_area_cirlce.py
+
+# structure of unit testing
+# import statements
+
+import unittest
+
+# import program / file you're testing
+import area_cirle
+
+# create a class that's going to do the testing
+class TestareaCircle(unittest.TestCase):
+    # run some tests that's going to give us the area of a cirlce
+    def test_areaCirlce_for_10radius(self):
+        # capture the results of the function 
+        # u need to know the formulae of what u are trying to achieve area = pi*r.squared
+        results = area_cirle.areaCirlce(10)
+
+        # check for  expected output
+        self.assertEqual(expected, results)
