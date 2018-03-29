6f3e3b7e4cc1fd8b50967e8e6fded0832201a9a8
gtsofa <jlsnyule@yahoo.com>
2018-03-29 19:44:26 +0300

    add commenting to employee.py

--- employee.py
+++ employee.py
@@ -6,20 +6,24 @@ class Employee:
     raise_amt = 1.05
 
     def __init__(self, first, last, pay):
+        """Initialize employee class"""
         self.first = first
         self.last = last
         self.pay = pay
 
     @property
     def email(self):
+        """Return employee email address"""
         return '{}.{}@email.com'.format(self.first, self.last)
 
     @property
     def fullname(self):
+        """Return employee full name"""
         return '{} {}'.format(self.first, self.last)
         
 
     def apply_raise(self):
+        """Calculates the employee pay raise"""
         self.pay = int(self.pay * self.raise_amt)
 
     fullname('julius', 'nyule', 20000)
