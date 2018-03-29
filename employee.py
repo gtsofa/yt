2ad6b1832b3f1961fe4a7800c24ac3bc2bddb29d
gtsofa <jlsnyule@yahoo.com>
2018-03-29 20:00:16 +0300

    refactor

--- employee.py
+++ employee.py
@@ -11,12 +11,12 @@ class Employee:
         self.last = last
         self.pay = pay
 
-    @property
+    #@property
     def email(self):
         """Return employee email address"""
-        return '{}.{}@email.com'.format(self.first, self.last)
+        return '{}.{}@company.com'.format(self.first.lower(), self.last.lower())
 
-    @property
+    #@property
     def fullname(self):
         """Return employee full name"""
         return '{} {}'.format(self.first, self.last)
@@ -26,4 +26,12 @@ class Employee:
         """Calculates the employee pay raise"""
         self.pay = int(self.pay * self.raise_amt)
 
-    fullname('julius', 'nyule', 20000)
+    #fullname('julius', 'nyule', 20000)
+
+    # testing the class
+emp_1 = Employee('Julius', 'Nyule', 5000)
+emp_2 = Employee('Tsofa', 'Maestro', 2000)
+
+print(emp_1) # <__main__.Employee instance at 0x7f9c1d9bda70>
+print(emp_1.fullname())
+print(emp_1.email())
