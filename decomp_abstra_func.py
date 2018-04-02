54cf7eea17fef70026d30c128072f02ae01bcf3c
gtsofa <jlsnyule@yahoo.com>
2018-04-03 00:51:56 +0300

    add initial commit

--- decomp_abstra_func.py
+++ decomp_abstra_func.py
@@ -1,2 +1,11 @@
 # decomp_abstract_func.py
-
+# Abstraction
+'''
+--> You don't need to know how project works to use it.
+--> Achieve it by -- function specifications or docstrings.
+'''
+# Decomposition
+'''
+--> Different devices will work together to achieve and end goal
+--> Achieve it by --modules --functions --classes
+'''
