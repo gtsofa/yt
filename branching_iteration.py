1620c17cfaddffe7508e4f70b228b72326c74d03
gtsofa <jlsnyule@yahoo.com>
2018-04-02 13:28:31 +0300

    add for loop comments

--- branching_iteration.py
+++ branching_iteration.py
@@ -8,19 +8,40 @@
 # text = str(input("type anything please"))
 # print(5*text)
 
-# if true prog ends and doesn't look at other conditions.
-x = float(input("Enter a value for x: "))
-y = float(input("Enter value for y: "))
+# # if true prog ends and doesn't look at other conditions.
+# x = float(input("Enter a value for x: "))
+# y = float(input("Enter value for y: "))
 
-if x == y :
-    print("x and y are equal.")
-    if y != 0:
-        print("therefore, x / y is ", x/y)
+# if x == y :
+#     print("x and y are equal.")
+#     if y != 0:
+#         print("therefore, x / y is ", x/y)
 
-elif x < y:
-    print("x is smaller")
+# elif x < y:
+#     print("x is smaller")
 
-else:
-    print("y is smaller")
+# else:
+#     print("y is smaller")
 
-print("Thanks")
+# print("Thanks")
+
+# n = raw_input("You're in the lost forest. Go right or left? ")
+
+# while n == "right":
+#     n = raw_input("You're in the lost forest. Go right or left? ")
+
+# print("Yaay. You got out of the lost forest. ")
+
+# for loops
+# syntax : 
+'''
+for <varibale> in range(<some number):
+    expression
+    expression
+    ...
+
+--> each time through the loop, <variable> takes a value
+--> first tiem, <varialbe> starts at the smallest value
+--> next time , <variable> gets the prev value + 1 
+--> etc
+'''
