77198add6ad81a4ed7c13310c081e0d136ab7914
gtsofa <jlsnyule@yahoo.com>
2018-04-03 19:17:08 +0300

    add another code

--- decomp_abstra_func.py
+++ decomp_abstra_func.py
@@ -14,18 +14,27 @@ def is_even_with_return(i):
     """
     Returns True is i is even, otherwise false.
     """
-    print("print with return")
+    print("with return")
     remainder = i % 2
-    return remainder == 0
+    return remainder == 0 # to make it boolean
 
 # calling the function now
 print(is_even_with_return(3))
 
-# without return
-def is_even_with_no_return(i):
-    print("without return")
-    remainder = i % 2
-    # return None
+# using the function later on the code
+print("all numbers between 0 and 20: even or not")
+for i in range(20):
+    if is_even_with_return(i):
+        print(i, "even")
 
-is_even_with_no_return(3)
-print(is_even_with_no_return(3))
+    else:
+        print(i, "odd")
+
+# # without return
+# def is_even_with_no_return(i):
+#     print("without return")
+#     remainder = i % 2
+#     # return None
+
+# is_even_with_no_return(3)
+# print(is_even_with_no_return(3))
