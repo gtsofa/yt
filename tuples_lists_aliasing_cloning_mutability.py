9dbe088df324d842a94cbbee682b565edd6c1b1a
gtsofa <jlsnyule@yahoo.com>
2018-04-03 23:39:05 +0300

    add comments to lists

--- tuples_lists_aliasing_cloning_mutability.py
+++ tuples_lists_aliasing_cloning_mutability.py
@@ -1,1 +1,19 @@
 # tuples_listing_aliasing_cloning_mutability.py
+
+def quotient_and_remainder(x, y):
+    """
+    Returns the quotient and remainder 
+    """
+    q = x // y
+    r = x % y
+
+    return (q, r)
+
+(quot, rem) = quotient_and_remainder(4,5)
+print(quot, rem)
+
+# FYI about Lists
+'''
+--> lists are indexed 0 to len(L)-1
+--> range(n) goes from 0 to n-1
+'''
