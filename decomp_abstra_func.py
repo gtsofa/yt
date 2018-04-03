a2f6af984017e3876eeab5e8a6f0e618b3afc0f8
gtsofa <jlsnyule@yahoo.com>
2018-04-03 20:00:34 +0300

    function as arguments

--- decomp_abstra_func.py
+++ decomp_abstra_func.py
@@ -10,25 +10,25 @@
 --> Achieve it by --modules --functions --classes
 '''
 
-def is_even_with_return(i):
-    """
-    Returns True is i is even, otherwise false.
-    """
-    print("with return")
-    remainder = i % 2
-    return remainder == 0 # to make it boolean
+# def is_even_with_return(i):
+#     """
+#     Returns True is i is even, otherwise false.
+#     """
+#     print("with return")
+#     remainder = i % 2
+#     return remainder == 0 # to make it boolean
 
-# calling the function now
-print(is_even_with_return(3))
+# # calling the function now
+# print(is_even_with_return(3))
 
-# using the function later on the code
-print("all numbers between 0 and 20: even or not")
-for i in range(20):
-    if is_even_with_return(i):
-        print(i, "even")
+# # using the function later on the code
+# print("all numbers between 0 and 20: even or not")
+# for i in range(20):
+#     if is_even_with_return(i):
+#         print(i, "even")
 
-    else:
-        print(i, "odd")
+#     else:
+#         print(i, "odd")
 
 # # without return
 # def is_even_with_no_return(i):
@@ -38,3 +38,21 @@ for i in range(20):
 
 # is_even_with_no_return(3)
 # print(is_even_with_no_return(3))
+
+# function as arguments
+
+def func_a():
+    print 'inside a func_a'
+
+def func_b(y):
+    print 'inside func_b'
+    return y
+
+def func_c(z):
+    print 'inside func_c'
+    return z()
+
+# testing them out now
+print func_a()
+print 5 + func_b(2)
+print func_c(func_a)
