be659d7ea2be23ebd8b534b5d865e509bdc904b4
gtsofa <jlsnyule@yahoo.com>
2018-04-03 19:07:07 +0300

    add is_even_function

--- decomp_abstra_func.py
+++ decomp_abstra_func.py
@@ -9,3 +9,23 @@
 --> Different devices will work together to achieve and end goal
 --> Achieve it by --modules --functions --classes
 '''
+
+def is_even_with_return(i):
+    """
+    Returns True is i is even, otherwise false.
+    """
+    print("print with return")
+    remainder = i % 2
+    return remainder == 0
+
+# calling the function now
+print(is_even_with_return(3))
+
+# without return
+def is_even_with_no_return(i):
+    print("without return")
+    remainder = i % 2
+    # return None
+
+is_even_with_no_return(3)
+print(is_even_with_no_return(3))
