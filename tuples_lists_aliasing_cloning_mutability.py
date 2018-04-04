ed33aa1a1d51f2d25f14f39e29b1df471a3094ae
gtsofa <jlsnyule@yahoo.com>
2018-04-04 20:42:34 +0300

    add implementation code

--- tuples_lists_aliasing_cloning_mutability.py
+++ tuples_lists_aliasing_cloning_mutability.py
@@ -1,20 +1,36 @@
 # tuples_listing_aliasing_cloning_mutability.py
 
 # FYI: using tuples to return more than 1 values in a func
-def quotient_and_remainder(x, y):
-    """
-    Returns the quotient and remainder 
-    """
-    q = x // y
-    r = x % y
+# def quotient_and_remainder(x, y):
+#     """
+#     Returns the quotient and remainder 
+#     """
+#     q = x // y
+#     r = x % y
 
-    return (q, r)
+#     return (q, r)
 
-(quot, rem) = quotient_and_remainder(4,5)
-print(quot, rem)
+# (quot, rem) = quotient_and_remainder(4,5)
+# print(quot, rem)
 
-# FYI about Lists
-'''
---> lists are indexed 0 to len(L)-1
---> range(n) goes from 0 to n-1
-'''
+# # FYI about Lists
+# '''
+# --> lists are indexed 0 to len(L)-1
+# --> range(n) goes from 0 to n-1
+# '''
+
+# manupulating tuples
+# can iterate over values
+
+def get_data(aTuple):
+    nums = ()
+    words = ()
+    for t in aTuple:
+        nums = nums + (t[0],) # (t[0],) implies a tuple with only 1 element!
+        if t[1] not in words:
+            words = words + (t[1],)
+
+    min_n = min(nums)
+    max_n = max(nums)
+    unique_words = len(words)
+    return (min_n, max_n, unique_words)
