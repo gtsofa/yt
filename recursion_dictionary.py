b72d37ac8045fb7cadcecf1f406835357e53ecbe
gtsofa <jlsnyule@yahoo.com>
2018-04-06 23:59:14 +0300

    add comments to code

--- recursion_dictionary.py
+++ recursion_dictionary.py
@@ -6,19 +6,32 @@
 # check a*b --> 'add a to itself b times'
 # capture state by
 """
-start = i
-stop at = 0
-condition: i < i-1 # each time it reduces by 1 iteratively until i get down to 0
-result = current value of computation
-"""
-def mult_iter(a, b):
-    # returns a * b using iteration Eg 2*5 --> 2+2+2+2+2
-    result = 0
-    while b > 0:
-        result += a # adds a to results in every iteration
-        b -= 1 # step thro by going backward --> 5 then 4 then 3 then 2 then 1
+# start = i
+# stop at = 0
+# condition: i < i-1 # each time it reduces by 1 iteratively until i get down to 0
+# result = current value of computation
+# """
+# def mult_iter(a, b):
+#     # returns a * b using iteration Eg 2*5 --> 2+2+2+2+2
+#     result = 0
+#     while b > 0:
+#         result += a # adds a to results in every iteration
+#         b -= 1 # step thro by going backward --> 5 then 4 then 3 then 2 then 1
 
-    return result
+#     return result
 
-# testing here:
-print(mult_iter(2,5))
+# # testing here:
+# print(mult_iter(2,5))
+
+# get the factorial of a number
+# Q - What's the base case?
+# A - when n == 1 --> this is the simplest bit i know/can deal with
+# when will it stop at --> n-1
+
+def fact(n):
+    """Return the factorial of a number."""
+    if n == 1:
+        return 1
+
+    else:
+        return n * fact(n-1)
