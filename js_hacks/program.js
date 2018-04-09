9796f8518e90571727c7d22589ee2a7821fa42dc
gtsofa <jlsnyule@yahoo.com>
2018-04-09 22:36:51 +0300

    add control statement

--- js_hacks/program.js
+++ js_hacks/program.js
@@ -1,3 +1,21 @@
 // program.js
 
 document.write("Hello World!")
+
+var age = 17;
+
+// if statement
+/* if (condition){
+    //statements
+}
+else {
+    //statements.
+} */
+if (age >= 18) {
+    document.write("Go the registrar of person for your ID." + </br>);
+
+
+}
+else{
+    document.write("You're under 18")
+}
