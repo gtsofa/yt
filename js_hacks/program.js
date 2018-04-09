dccf5ff513cffd7ed63987a661afc3a7ff81db31
gtsofa <jlsnyule@yahoo.com>
2018-04-10 00:32:06 +0300

    refactor

--- js_hacks/program.js
+++ js_hacks/program.js
@@ -91,9 +91,17 @@ function change(){
 
 }
 */
+var image_tracker = 't';
 function change(){
     var image = document.getElementById('social');
-    image.src = "twitter.png"
+    if (image_tracker == 'f'){
+        image.src = 'twitter.png';
+        image_tracker = 't';
+    }
+    else{
+        image.src = 'facebook.png';
+        image_tracker = 'f'
+    }
     
     
 }
