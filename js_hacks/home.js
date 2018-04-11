082fffb28f5ae4fec80214d4b7bc44218a7b8714
gtsofa <jlsnyule@yahoo.com>
2018-04-11 21:28:28 +0300

    string literals

--- js_hacks/home.js
+++ js_hacks/home.js
@@ -1,1 +1,8 @@
 // home.js
+
+var button = document.querySelector('button');
+
+button.onclick = function(){
+    var name = prompt("What is your name? ");
+    alert("hello, "+name+" nice to see you.");
+}
