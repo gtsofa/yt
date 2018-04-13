c51072fa417241073a51a4e3f3bdf4114e5959fe
gtsofa <jlsnyule@yahoo.com>
2018-04-14 00:34:56 +0300

    add other except clauses

--- test_debugging.py
+++ test_debugging.py
@@ -3,8 +3,14 @@ try:
 
     a = int(input("Enter a number: "))
     b = int(input("enter another number: "))
-    print(a/b)
+    print("a/b: ", a/b)
 
+
+except ValueError:
+    print("Could not convert to a number.")
+
+except ZeroDivisionError:
+    print("Can't divide by zero. ")
 except:
     print("Bug in user input.")
 
