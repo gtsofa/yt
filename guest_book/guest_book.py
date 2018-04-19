f7a8cba3fd85f61158eea0d8a9cdd8e7daeadb69
gtsofa <jlsnyule@yahoo.com>
2018-04-20 00:03:08 +0300

    add changes

--- guest_book/guest_book.py
+++ guest_book/guest_book.py
@@ -1,1 +1,18 @@
 #guest_book.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# create a route - decorate and the action
+@app.route('/')
+def index():
+
+    return '<h1>Hello There!</h1>'
+
+@app.route('/home')
+def home():
+    return render_template('example.html', myvar='I love jinja')
+
+if __name__=='__main__':
+    app.run(debug=True)
+
